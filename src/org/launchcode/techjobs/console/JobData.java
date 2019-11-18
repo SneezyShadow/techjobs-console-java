3eb01e218461e3977a498b9b24312bda1a2dcb90
SneezyShadow <sneezyshadow@gmail.com>
2019-11-18 12:20:13 -0600

    Part One Finished

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -76,7 +77,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toUpperCase().contains(value.toUpperCase())) {
                 jobs.add(row);
             }
         }
@@ -123,6 +124,22 @@ public class JobData {
             System.out.println("Failed to load job data");
             e.printStackTrace();
         }
+    }
+    public static ArrayList<HashMap<String, String>> findByValue(String value){
+
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for(int i = 0; i < allJobs.size(); i++){
+            for(String values : allJobs.get(i).values())
+
+                if(values.toUpperCase().contains(value.toUpperCase())){
+                    jobs.add(allJobs.get(i));
+                }
+        }
+
+        return jobs;
     }
 
 }
